e76981e377c996469b519ad31c8f025609d4e541
Allarion <Allarion@allevil.de>
2026-02-28 19:46:00 +0100

    iam-security-spring: added README.md as overview

--- src/main/java/de/javaholic/toolkit/adminworkbench/AdminWorkbenchApplication.java
+++ src/main/java/de/javaholic/toolkit/adminworkbench/AdminWorkbenchApplication.java
@@ -3,7 +3,9 @@ package de.javaholic.toolkit.adminworkbench;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @ComponentScan("de.javaholic.toolkit")
 public class AdminWorkbenchApplication {
